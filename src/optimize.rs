959a69dfeeb598075148b40d85229dd3fbe4f550
Kento Sugama <kento.sugama@dfinity.org>
2023-04-06 10:58:57 -0700

    scaffold

--- src/optimize.rs
+++ src/optimize.rs
@@ -7,6 +7,8 @@ pub fn optimize(m: &mut Module, keep_name_section: bool) {
     let temp_file_name = "temp.opt.wasm";
     let temp_path = PathBuf::from(temp_file_name);
 
+    // FIXME extract out the custom section first
+
     // write to fs
     m.emit_wasm_file(temp_file_name).unwrap();
 
@@ -14,6 +16,8 @@ pub fn optimize(m: &mut Module, keep_name_section: bool) {
     OptimizationOptions::new_opt_level_3()
         .run(temp_file_name, temp_file_name)
         .unwrap();
+
+    // FIXME re-insert the custom section before assigning back to m
 
     // read back in from fs and assign back to m
     *m = parse_wasm_file(temp_path, keep_name_section).unwrap();

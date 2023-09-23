8b16ddd5d135cb1f827a57798e72680b837a8f79
Yan Chen <yan.chen@dfinity.org>
2023-09-23 13:02:43 -0700

    fix

--- src/instrumentation.rs
+++ src/instrumentation.rs
@@ -6,6 +6,7 @@ use std::collections::HashSet;
 
 const METADATA_SIZE: i32 = 24;
 const DEFAULT_PAGE_LIMIT: i32 = 30;
+const LOG_ITEM_SIZE: i32 = 12;
 
 struct InjectionPoint {
     position: usize,
@@ -124,11 +125,10 @@ pub fn instrument(m: &mut Module, config: Config) -> Result<(), String> {
         //inject_start(m, vars.is_init);
         inject_init(m, vars.is_init);
     }
-    // Only persist globals when stable memory is preallocated
-    if config.is_preallocated() {
-        inject_pre_upgrade(m, &vars, &config);
-        inject_post_upgrade(m, &vars, &config);
-    }
+    // Persist globals
+    inject_pre_upgrade(m, &vars, &config);
+    inject_post_upgrade(m, &vars, &config);
+
     inject_canister_methods(m, &vars);
     let leb = make_leb128_encoder(m);
     make_stable_getter(m, &vars, leb, &config);
@@ -531,10 +531,10 @@ fn make_printer(m: &mut Module, vars: &Variables, writer: FunctionId) -> Functio
                 .global_get(vars.total_counter)
                 .store(memory, StoreKind::I64 { atomic: false }, MemArg { offset: 0, align: 8 })
                 .global_get(vars.log_size)
-                .i32_const(12)
+                .i32_const(LOG_ITEM_SIZE)
                 .binop(BinaryOp::I32Mul)
                 .i32_const(0)
-                .i32_const(12)
+                .i32_const(LOG_ITEM_SIZE)
                 .call(writer)
                 // restore memory
                 .i32_const(0)
@@ -607,22 +607,10 @@ fn inject_init(m: &mut Module, is_init: GlobalId) {
 fn inject_pre_upgrade(m: &mut Module, vars: &Variables, config: &Config) {
     let writer = get_ic_func_id(m, "stable_write");
     let memory = get_memory_id(m);
-    let a = m.locals.add(ValType::I64);
-    let b = m.locals.add(ValType::I64);
-    let c = m.locals.add(ValType::I64);
     let mut builder = get_or_create_export_func(m, "canister_pre_upgrade");
     #[rustfmt::skip]
     builder
-        // backup memory. This is not strictly needed, since it's at the end of pre-upgrade.
-        .i32_const(0)
-        .load(memory, LoadKind::I64 { atomic: false }, MemArg { offset: 0, align: 8})
-        .local_set(a)
-        .i32_const(8)
-        .load(memory, LoadKind::I64 { atomic: false }, MemArg { offset: 0, align: 8})
-        .local_set(b)
-        .i32_const(16)
-        .load(memory, LoadKind::I64 { atomic: false }, MemArg { offset: 0, align: 8})
-        .local_set(c)
+        // no need to backup memory, since this is the end of the pre-upgrade.
         // persist globals
         .i32_const(0)
         .global_get(vars.total_counter)
@@ -642,17 +630,7 @@ fn inject_pre_upgrade(m: &mut Module, vars: &Variables, config: &Config) {
         .i32_const(config.metadata_start_address())
         .i32_const(0)
         .i32_const(METADATA_SIZE)
-        .call(writer)
-        // restore memory
-        .i32_const(0)
-        .local_get(a)
-        .store(memory, StoreKind::I64 { atomic: false }, MemArg { offset: 0, align: 8 })
-        .i32_const(8)
-        .local_get(b)
-        .store(memory, StoreKind::I64 { atomic: false }, MemArg { offset: 0, align: 8 })
-        .i32_const(16)
-        .local_get(c)
-        .store(memory, StoreKind::I64 { atomic: false }, MemArg { offset: 0, align: 8 });
+        .call(writer);
 }
 fn inject_post_upgrade(m: &mut Module, vars: &Variables, config: &Config) {
     let reader = get_ic_func_id(m, "stable_read");
@@ -733,12 +711,12 @@ fn make_stable_getter(m: &mut Module, vars: &Variables, leb: FunctionId, config:
         .i32_const(0)
         .i32_const(config.log_start_address())
         .global_get(vars.log_size)
-        .i32_const(12)
+        .i32_const(LOG_ITEM_SIZE)
         .binop(BinaryOp::I32Mul)
         .call(reader)
         .i32_const(0)
         .global_get(vars.log_size)
-        .i32_const(12)
+        .i32_const(LOG_ITEM_SIZE)
         .binop(BinaryOp::I32Mul)
         .call(reply_data)
         .call(reply);

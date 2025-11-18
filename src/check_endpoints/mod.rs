567ae9024b216faece67765fb884ad45275dbfd9
Louis Pahlavi <louis.pahlavi@dfinity.org>
2025-11-18 09:14:14 +0100

    DEFI-2488: Add support for comments in hidden endpoints

--- src/check_endpoints/mod.rs
+++ src/check_endpoints/mod.rs
@@ -4,9 +4,8 @@ pub use crate::check_endpoints::candid::CandidParser;
 use crate::{info::ExportedMethodInfo, utils::get_exported_methods};
 use anyhow::anyhow;
 use parse_display::{Display, FromStr};
-use std::io::{BufRead, BufReader};
-use std::str::FromStr;
-use std::{collections::BTreeSet, path::Path};
+use std::io::BufReader;
+use std::{collections::BTreeSet, io::BufRead, path::Path, str::FromStr};
 use walrus::Module;
 
 #[derive(Clone, Eq, Debug, Ord, PartialEq, PartialOrd, Display, FromStr)]
@@ -112,22 +111,57 @@ pub fn check_endpoints(
 
 fn read_hidden_endpoints(maybe_path: Option<&Path>) -> anyhow::Result<BTreeSet<CanisterEndpoint>> {
     if let Some(path) = maybe_path {
-        let file = std::fs::File::open(path)
-            .map_err(|e| anyhow!("Failed to read hidden endpoints file: {e:?}"))?;
-        let reader = BufReader::new(file);
-        let lines = reader
-            .lines()
-            .collect::<Result<Vec<_>, _>>()
-            .map_err(|e| anyhow!("Failed to read hidden endpoints file: {e:?}"))?;
-        let endpoints = lines
-            .iter()
-            .map(|line| line.trim())
-            .filter(|line| !line.is_empty())
-            .map(CanisterEndpoint::from_str)
-            .collect::<Result<BTreeSet<_>, _>>()
-            .map_err(|e| anyhow!("Failed to parse hidden endpoints from file: {e:?}"))?;
+        let mut endpoints = BTreeSet::new();
+        for line in read_lines(path)? {
+            if let Some(endpoint) = parse_line(line)? {
+                endpoints.insert(endpoint);
+            }
+        }
         Ok(endpoints)
     } else {
         Ok(BTreeSet::new())
     }
+}
+
+fn read_lines(path: &Path) -> anyhow::Result<Vec<String>> {
+    let file = std::fs::File::open(path)
+        .map_err(|e| anyhow!("Could not open hidden endpoints file: {e:?}"))?;
+
+    let reader = BufReader::new(file);
+    let mut lines = Vec::new();
+
+    for line in reader.lines() {
+        let line = line?;
+        let trimmed = line.trim();
+        if !trimmed.is_empty() {
+            lines.push(trimmed.to_string());
+        }
+    }
+
+    Ok(lines)
+}
+
+fn parse_line(line: String) -> anyhow::Result<Option<CanisterEndpoint>> {
+    fn parse_uncommented_line(line: String) -> anyhow::Result<Option<CanisterEndpoint>> {
+        CanisterEndpoint::from_str(line.as_str())
+            .map(Some)
+            .map_err(Into::into)
+    }
+    // Comment: ignore line
+    if line.starts_with("#") {
+        return Ok(None);
+    }
+    // Quoted line: use JSON string syntax to allow for character escaping
+    if line.starts_with('"') {
+        if !line.ends_with('"') || line.len() < 2 {
+            return Err(anyhow!(
+                "Could not parse hidden endpoint, missing terminating quote: {line}"
+            ));
+        }
+        return serde_json::from_str::<String>(&line)
+            .map_err(|e| anyhow!("Could not parse hidden endpoint: {e:?}"))
+            .and_then(parse_uncommented_line);
+    }
+    // Regular line
+    parse_uncommented_line(line)
 }
